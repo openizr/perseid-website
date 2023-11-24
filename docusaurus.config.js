5524664828c81a62b57b1c6e9b4e05209b0f9e49
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2023-11-24 12:28:15 +0100

    Update SEO metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,22 +6,20 @@
 
 import { themes as prismThemes } from 'prism-react-renderer';
 
+
+// // Declare some <meta> tags
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Perseid',
-  tagline: 'Back and front ends as a configuration',
+  tagline: 'Prototype and build full-stack, production-ready web apps in minutes',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://perseid.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -43,16 +41,15 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/openizr/perseid-website/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -67,93 +64,120 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      colorMode: {
-        defaultMode: 'dark',
-        disableSwitch: true,
-        respectPrefersColorScheme: false,
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
+    // Replace with your project's social card
+    image: 'img/social-card.png',
+    metadata: [
+      { name: 'keywords', content: 'front-end,back-end,prototype,javascript,typescript,framework,web,app' },
+    ],
+    // headTags: [
+    //   // Declare a <link> preconnect tag
+    //   {
+    //     tagName: 'link',
+    //     attributes: {
+    //       rel: 'preconnect',
+    //       href: 'https://example.com',
+    //     },
+    //   },
+    //   // Declare some json-ld structured data
+    //   {
+    //     tagName: 'script',
+    //     attributes: {
+    //       type: 'application/ld+json',
+    //     },
+    //     innerHTML: JSON.stringify({
+    //       '@context': 'https://schema.org/',
+    //       '@type': 'Organization',
+    //       name: 'Meta Open Source',
+    //       url: 'https://opensource.fb.com/',
+    //       logo: 'https://opensource.fb.com/img/logos/Meta-Open-Source.svg',
+    //     }),
+    //   },
+    // ],
+    navbar: {
+      title: 'Perseid',
+      logo: {
+        alt: 'Perseid logo',
+        src: 'img/logo.svg',
       },
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'Perseid',
-        logo: {
-          alt: 'Perseid logo',
-          src: 'img/logo.svg',
+      items: [
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'conceptsSidebar',
+        //   position: 'left',
+        //   label: 'Concepts',
+        // },
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'tutorialSidebar',
+        //   position: 'left',
+        //   label: 'Tutorial',
+        // },
+        {
+          type: 'docSidebar',
+          sidebarId: 'apiSidebar',
+          position: 'left',
+          label: 'API',
         },
-        items: [
-          // {
-          //   type: 'docSidebar',
-          //   sidebarId: 'conceptsSidebar',
-          //   position: 'left',
-          //   label: 'Concepts',
-          // },
-          // {
-          //   type: 'docSidebar',
-          //   sidebarId: 'tutorialSidebar',
-          //   position: 'left',
-          //   label: 'Tutorial',
-          // },
-          {
-            type: 'docSidebar',
-            sidebarId: 'apiSidebar',
-            position: 'left',
-            label: 'API',
-          },
-          // { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/openizr/perseid',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'API',
-                to: '/docs/api/dev-kit/config',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/perseid',
-              },
-              // {
-              //   label: 'Discord',
-              //   href: 'https://discordapp.com/invite/docusaurus',
-              // },
-              // {
-              //   label: 'Twitter',
-              //   href: 'https://twitter.com/docusaurus',
-              // },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/openizr/perseid',
-              },
-            ],
-          },
-        ],
-        copyright: `Released under the MIT License.<br/>Copyright (c) ${new Date().getFullYear()} Openizr.`,
-      },
-      prism: {
-        theme: prismThemes.dracula,
-      },
-    }),
+        // { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/openizr/perseid',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'API',
+              to: '/docs/api/dev-kit/config',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/perseid',
+            },
+            // {
+            //   label: 'Discord',
+            //   href: 'https://discordapp.com/invite/docusaurus',
+            // },
+            // {
+            //   label: 'Twitter',
+            //   href: 'https://twitter.com/docusaurus',
+            // },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/openizr/perseid',
+            },
+          ],
+        },
+      ],
+      copyright: `Released under the MIT License.<br/>Copyright (c) ${new Date().getFullYear()} Openizr.`,
+    },
+    prism: {
+      theme: prismThemes.dracula,
+    },
+  },
 };
 
 export default config;
